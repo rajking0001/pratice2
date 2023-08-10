49213a762b3a3433dbc19659954816896c3e0e68
rajking0001 <127574648+rajking0001@users.noreply.github.com>
2023-08-11 01:35:19 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-const app=require("./app");
-const { connectDatabase } = require("./config/database");
-connectDatabase();
-
-app.listen(process.env.PORT,() => {
-    console.log(`server is running on port ${process.env.PORT}`);
-});
